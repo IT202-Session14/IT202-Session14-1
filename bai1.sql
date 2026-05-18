be30c544a2e3cec3b455fc3e09621083a45caec4
Vu Dinh Anh Tuan <vudinhanhtuan26@gmail.com>
2026-05-19 02:13:26 +0700

    M

--- bai1.sql
+++ bai1.sql
@@ -8,11 +8,10 @@ SELECT * FROM Patient_Invoices WHERE patient_id = 1;
 DELIMITER //
 CREATE PROCEDURE PayHopitalFee(IN p_patient_id INT, IN p_amout DECIMAL(18,2))
 BEGIN 
-	DECLARE EXIT HANDLER FOR SQLEXCEPTION
-    BEGIN 
-    ROLLBACK;
-    END;
 	START TRANSACTION;
+	IF (SELECT balance FROM Wallets WHERE patient_id = p_patient_id) < p_amout THEN
+    ROLLBACK;
+    ELSE 
     UPDATE Wallets
     SET balance = balance - p_amout
     WHERE patient_id = p_patient_id;
@@ -20,5 +19,6 @@ BEGIN
     SET total_due = total_due - p_amout
     WHERE patient_id = p_patient_id;
     COMMIT;
+    END IF;
 END //
 DELIMITER ;
